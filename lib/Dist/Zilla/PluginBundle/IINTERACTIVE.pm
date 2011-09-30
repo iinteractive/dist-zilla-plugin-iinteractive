b1dc97b738b15016fbded5385e6b846b71acf662
Cory G Watson <gphat@Corys-iMac.local>
2011-09-30 14:54:18 -0500

    First commit.

---
+++ lib/Dist/Zilla/PluginBundle/IINTERACTIVE.pm
@@ -0,0 +1,107 @@
+package Dist::Zilla::PluginBundle::IINTERACTIVE;
+use Moose;
+
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
+# ABSTRACT: Dist::Zilla plugins for gphat
+
+=head1 DESCRIPTION
+
+This is the plugin bundle that Infinity Interactive uses.  It's equivalent to:
+
+    [@Basic]
+
+    [Git::NextVersion]
+    first_version   = 0.01
+    version_regexp  = ^(\d+\.\d+)$
+
+    [ReadmeMarkdownFromPod]
+
+    [PkgVersion]
+
+    [AutoPrereqs]
+
+    [NextRelease]
+    format          = %v %{MMM d yyyy}d
+
+    [SynopsisTests]
+
+    [PodSyntaxTests]
+
+    [MetaJSON]
+
+    [GithubMeta]
+
+    [CopyFilesFromBuild]
+    copy            = README.mkdn
+
+    [PruneFiles]
+    filenames       = dist.ini
+
+    [Git::Commit]
+
+    [Git::Tag]
+    tag_format      = %v
+
+    [PodWeaver]
+
+=cut
+
+sub configure {
+    my ($self) = @_;
+
+    $self->add_bundle('@Basic');
+
+    $self->add_plugins([
+        'Git::NextVersion' => {
+            first_version  => '0.01',
+            version_regexp => '^(\d+\.\d+)$'
+        }
+    ]);
+
+    $self->add_plugins(qw(
+        ReadmeMarkdownFromPod
+        PkgVersion
+        AutoPrereqs
+    ));
+
+    $self->add_plugins([
+        'NextRelease' => {
+            format => '%v %{MMM d yyyy}d'
+        }
+    ]);
+
+    $self->add_plugins(qw(
+        SynopsisTests
+        PodSyntaxTests
+        MetaJSON
+        GithubMeta
+    ));
+
+    $self->add_plugins([
+        'CopyFilesFromBuild' => {
+            format => 'README.mkdn'
+        }
+    ]);
+    
+    $self->add_plugins([
+        'PruneFiles' => {
+            filenames => [ qw(dist.ini weaver.ini) ]
+        }
+    ]);
+
+    $self->add_plugins(qw(Git::Commit));
+
+    $self->add_plugins([
+        'Git::Tag' => {
+            tag_format => '%v'
+        }
+    ]);
+
+    $self->add_plugins(qw(PodWeaver));
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
